340fb87aac0b61628f71ac438e723a309557ac6c
Manas Rajani <manasrajanidy89@gmail.com>
2026-02-17 12:52:33 +0530

    Add Project Phase 3 Part b

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require('../models/listing')
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const mapBoxToken = process.env.MAP_BOX_TOKEN
+const geocodingClient = mbxGeocoding({ accessToken: mapBoxToken })
 
 module.exports.index = async (req, res) => {
   const alllisting = await Listing.find({})
@@ -27,16 +30,26 @@ module.exports.showListing = async (req, res) => {
 }
 
 module.exports.createListing = async (req, res, next) => {
-  let url = req.file ? req.file.path : null; // Get the image URL from Cloudinary if uploaded
-  let filename = req.file ? req.file.filename : null; // Get the filename from Cloudinary if uploaded
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location, // Use the location from the form input
+      limit: 1
+    })
+    .send()
+
+  let url = req.file ? req.file.path : null // Get the image URL from Cloudinary if uploaded
+  let filename = req.file ? req.file.filename : null // Get the filename from Cloudinary if uploaded
   const newListing = new Listing(req.body.listing) // Fixed: use req.body.listing
   newListing.owner = req.user._id // Set the owner to the logged-in user
-  // if (url && filename) {
+  if (url && filename) {
     newListing.image = {
       url: url,
       filename: filename
     }
-  // }
+  }
+  
+              newListing.geometry = response.body.features[0].geometry // Set the geometry from Mapbox response
+
   await newListing.save()
   req.flash('success', 'Successfully created a new listing!')
   res.redirect(`/listings`)
@@ -49,31 +62,31 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash('error', 'Listing you are looking for does not exist!')
     return res.redirect('/listings')
   }
-  let originalImageUrl =  listing.image.url;
-  console.log(originalImageUrl + "original image url");  
-  originalImageUrl = originalImageUrl.replace('/upload', '/upload/w_250'); // Resize the image to width 300px
+  let originalImageUrl = listing.image.url
+  console.log(originalImageUrl + 'original image url')
+  originalImageUrl = originalImageUrl.replace('/upload', '/upload/w_250') // Resize the image to width 300px
   res.render('listings/edit.ejs', { listing, originalImageUrl })
 }
 
 module.exports.updateListing = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   const listing = await Listing.findByIdAndUpdate(id, req.body.listing, {
     runValidators: true,
     new: true
-  });
+  })
   if (req.file) {
-    const url = req.file.path;
-    const filename = req.file.filename;
-    listing.image = { url, filename };
-    await listing.save();
+    const url = req.file.path
+    const filename = req.file.filename
+    listing.image = { url, filename }
+    await listing.save()
   }
-  req.flash('success', 'Listing updated successfully!');
-  res.redirect(`/listings/${id}`);
+  req.flash('success', 'Listing updated successfully!')
+  res.redirect(`/listings/${id}`)
 }
 
 module.exports.deleteListing = async (req, res) => {
-    const { id } = req.params
-    await Listing.findByIdAndDelete(id)
-    req.flash('success', 'Listing deleted successfully!')
-    res.redirect('/listings')
-  }
+  const { id } = req.params
+  await Listing.findByIdAndDelete(id)
+  req.flash('success', 'Listing deleted successfully!')
+  res.redirect('/listings')
+}
--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,14 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
+  // style: 'mapbox://styles/mapbox/outdoors-v12',
   style: 'mapbox://styles/mapbox/streets-v12',
-  center: [76.35, 21.83], // [longitude, latitude]
-  zoom: 9
+  center: listing.geometry.coordinates, // Use the listing's coordinates to center the map
+  zoom: 10
 });
+
+const marker = new mapboxgl.Marker({color: 'red'})
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25})
+  .setHTML(`<h4>${listing.location}</h4><p>Exact Location will be provided after booking</p>`)) // Add a popup with the listing location
+  .addTo(map);
